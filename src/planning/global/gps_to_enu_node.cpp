ef51806e0167c709c9682f1c109f4380b4f7c7ae
Chris Goodin <cgoodin@cavs.msstate.edu>
2022-02-04 13:28:14 -0600

    Added test for transform subscription

--- src/planning/global/gps_to_enu_node.cpp
+++ src/planning/global/gps_to_enu_node.cpp
@@ -20,8 +20,6 @@
 
 int main(int argc, char **argv){
 
-    
-
     auto n = avt_341::node::init_node(argc,argv,"path_manager");
 
     auto path_pub = n->create_publisher<avt_341::msg::Path>("avt_341/enu_waypoints", 10);
@@ -36,8 +34,6 @@ int main(int argc, char **argv){
     }
 
     std::vector< std::vector<double> > path;
-    double utm_east = 333990.45;
-    double utm_north = 3704867.39;
     avt_341::coordinate_system::CoordinateConverter converter;
     std::vector<avt_341::coordinate_system::UTM> utm_waypoints;
     for (int i=0;i<gps_waypoints_lat.size();i++){
@@ -46,7 +42,6 @@ int main(int argc, char **argv){
         gps_wp.longitude = gps_waypoints_lon[i];
         gps_wp.altitude = 100.0f; // approximate elevation for Starkville, MS
         avt_341::coordinate_system::UTM utm_wp = converter.LLA2UTM(gps_wp);
-        std::cout<<"POINT "<<i<<" "<<gps_waypoints_lat[i]<<" "<<gps_waypoints_lon[i]<<" "<<utm_wp.x<<" "<<utm_wp.y<<std::endl;
         utm_waypoints.push_back(utm_wp);
         std::vector<double> point;
         point.push_back(utm_wp.x);
@@ -64,19 +59,22 @@ int main(int argc, char **argv){
     int count = 0;
     while(avt_341::node::ok()){
 
-        //std::vector<std::vector<float>> path = {{0, 10}, {10, 20}, {10, 50}};
+
         tf::StampedTransform transform;
-        listener.lookupTransform("/odom", "/utm",  ros::Time(0), transform);
-        std::cout<<transform.getOrigin().x() <<" "<<transform.getOrigin().y()<<std::endl;
+        try {
+            listener.lookupTransform("/odom", "/utm",  ros::Time(0), transform);
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+        }
         avt_341::msg::Path ros_path;
         ros_path.header.frame_id = "odom";
         ros_path.poses.clear();
         for (int32_t i = 0; i < path.size(); i++){
             avt_341::msg::PoseStamped pose;
-            //pose.pose.position.x = static_cast<float>(path[i][0]);
-            //pose.pose.position.y = static_cast<float>(path[i][1]);
-            pose.pose.position.x = path[i][0] - utm_east;
-            pose.pose.position.y = path[i][1] - utm_north;
+            pose.pose.position.x = path[i][0] + transform.getOrigin().x();
+            pose.pose.position.y = path[i][1] + transform.getOrigin().y();
             pose.pose.position.z = 0.0f;
             pose.pose.orientation.w = 1.0f;
             pose.pose.orientation.x = 0.0f;
